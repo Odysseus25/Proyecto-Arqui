47c83199bacf9549a4aab837b32d448a29c93ea6
dave <dave@David-PC>
2015-10-12 10:09:20 -0600

    Lógica con dos núcleos mejorada y mejor comentada

--- ProyectoArqui/src/proyectoarqui/ProyectoArqui.java
+++ ProyectoArqui/src/proyectoarqui/ProyectoArqui.java
@@ -29,8 +29,134 @@ public class ProyectoArqui {
      * @throws java.lang.InterruptedException
      */
     public static void main(String[] args) throws InterruptedException {
-        //Cargamos Memoria y variables
+        //Cargamos Memoria y colaEjecucion
         ConcurrentLinkedQueue<EstructuraHilo> colaEjecucion = new ConcurrentLinkedQueue<>();
+        Memoria mem = cargaMemoria(colaEjecucion);
+        
+        //Cargamos objetos de simulación
+        Bus busInstrucciones = new Bus(mem);
+        CyclicBarrier barrera = new CyclicBarrier(1);
+        Nucleo n1 = new Nucleo(1, 20, busInstrucciones, barrera);
+        Nucleo n2 = new Nucleo(2, 20, busInstrucciones, barrera);
+        
+        //Empezamos a simular!
+        int reloj=0;
+        //Booleanos que determinan si N1 y N2 están ejecutando algo
+        boolean finN1=false;
+        boolean finN2=false;
+        //Cargamos los hilos iniciales en N1 y N2
+        if(!colaEjecucion.isEmpty()) {
+            EstructuraHilo t1 = colaEjecucion.poll();
+            n1.setEstHilo(t1);
+            n1.setTerminado(false);
+        } else {
+            n1.setTerminado(true);
+        }
+        if(!colaEjecucion.isEmpty()) {
+            EstructuraHilo t2 = colaEjecucion.poll();
+            n2.setEstHilo(t2);
+            n2.setTerminado(false);
+        } else {
+            n2.setTerminado(true);
+        }
+        System.out.println("Reloj: "+reloj);
+        System.out.println("HID-N1: "+n1.getEstHilo().getHid()+", HPC-N1: "+n1.getEstHilo().getHpc());
+        System.out.println("HID-N2: "+n2.getEstHilo().getHid()+", HPC-N2: "+n2.getEstHilo().getHpc());
+        //Tiempo de cada CPU inicia en 0
+        int tiempo1=0;
+        int tiempo2=0;
+        //Si la cola no está vacía o aún se está ejecutando algún hilo, continúe ejecutando
+        while(!colaEjecucion.isEmpty()||!(finN1&&finN2)) {
+            System.out.println("Reloj: "+reloj);
+            
+            if(finN1&&finN2) {//Si ambos núcleos terminaron, carguemos hilos en cada uno (de ser posible)
+                if(!colaEjecucion.isEmpty()) {
+                    EstructuraHilo t1 = colaEjecucion.poll();
+                    n1.setEstHilo(t1);
+                    n1.setTerminado(false);
+                } else {//Si no hay nada que cargar para N1, márquelo terminado
+                    n1.setTerminado(true);
+                }
+                if(!colaEjecucion.isEmpty()) {
+                    EstructuraHilo t2 = colaEjecucion.poll();
+                    n2.setEstHilo(t2);
+                    n2.setTerminado(false);
+                } else {//Si no hay nada que cargar para N2, márquelo terminado
+                    n2.setTerminado(true);
+                }
+                tiempo1=0;
+                tiempo2=0;
+            } else if(finN1) {//Si solo N1 terminó, cargue solo N1
+                if(!colaEjecucion.isEmpty()) {
+                    EstructuraHilo t1 = colaEjecucion.poll();
+                    n1.setEstHilo(t1);
+                    n1.setTerminado(false);
+                } else {//Si no hay nada que cargar para N1, márquelo terminado
+                    n1.setTerminado(true);
+                }
+                tiempo1=0;
+            } else if(finN2) {//Si solo N2 terminó, cargue solo N2
+                if(!colaEjecucion.isEmpty()) {
+                    EstructuraHilo t2 = colaEjecucion.poll();
+                    n2.setEstHilo(t2);
+                    n2.setTerminado(false);
+                } else {//Si no hay nada que cargar para N2, márquelo terminado
+                    n2.setTerminado(true);
+                }
+                tiempo2=0;
+            } 
+            
+            //Si algún núcleo está terminado (no hay hilo que darle), no muestre su información
+            String n1Hid = (n1.isTerminado())?"Duerme":""+n1.getEstHilo().getHid();
+            String n2Hid = (n2.isTerminado())?"Duerme":""+n2.getEstHilo().getHid();
+            String n1Hpc = (n1.isTerminado())?"Duerme":""+n1.getEstHilo().getHpc();
+            String n2Hpc = (n2.isTerminado())?"Duerme":""+n2.getEstHilo().getHpc();
+            System.out.println("HID-N1: "+n1Hid+", HPC-N1: "+n1Hpc);
+            System.out.println("HID-N2: "+n2Hid+", HPC-N2: "+n2Hpc);
+        
+            //Si algún núcleo está terminado (no hay hilo que darle), no muestre su información
+            String regN1=(n1.isTerminado())?"Duerme":""+n1;
+            String regN2=(n2.isTerminado())?"Duerme":""+n2;
+            System.out.println("Nucleo 1: "+regN1+"\n"+"Nucleo 2: "+regN2);
+            
+            finN1=false;
+            finN2=false;
+            while(n1.Execute()&&n2.Execute()) {//Ejecuto instrucción por instrucción
+                reloj++;
+                tiempo1++;
+                tiempo2++;
+                if((tiempo1>=n1.getQuantum())&&!n1.isTerminado()) {//Si se acaba el quantum y no se ha terminado, guarde el hilo
+                    System.out.println("SE ACABÓ EL QUANTUM DE N1");
+                    n1.guardaHilo();
+                    colaEjecucion.add(n1.getEstHilo());
+                    finN1=true;
+                } 
+                if(tiempo2>=n2.getQuantum()&&!n2.isTerminado()) {//Si se acaba el quantum y no se ha terminado, guarde el hilo
+                    System.out.println("SE ACABÓ EL QUANTUM DE N2");
+                    n2.guardaHilo();
+                    colaEjecucion.add(n2.getEstHilo());
+                    finN2=true;
+                } 
+                if(finN1||finN2) {//Si se acabó uno de los dos quantums, salga del ciclo
+                    break;
+                }
+            }
+            //Determino si debo seguir o no el ciclo externo
+            finN1=!n1.isNoFin()||(tiempo1>=n1.getQuantum())||n1.isTerminado();
+            finN2=!n2.isNoFin()||(tiempo2>=n2.getQuantum())||n2.isTerminado();
+            //Si algún núcleo está terminado (no hay hilo que darle), no muestre su información
+            regN1=(n1.isTerminado())?"Duerme":""+n1;
+            regN2=(n2.isTerminado())?"Duerme":""+n2;
+            //Imprimo resultados de vuelta
+            System.out.println("Nucleo 1: "+regN1+"\n"+"Nucleo 2: "+regN2);
+            System.out.println("________________________________");
+        }
+        
+        
+    }
+    
+    //Carga la memoria pidiendo un directorio y leyendo cada archivo en ese directorio
+    public static Memoria cargaMemoria(ConcurrentLinkedQueue<EstructuraHilo> colaEjecucion) {
         Memoria mem = new Memoria(2);
         System.out.println("Ingrese el directorio de hilos: ");
         Scanner in = new Scanner(System.in);
@@ -68,128 +194,10 @@ public class ProyectoArqui {
         
         System.out.println(mem);
         System.out.println("largo de la cola: " + colaEjecucion.size());
-        
-        //Cargamos objetos de simulación
-        Bus busInstrucciones = new Bus(mem);
-        CyclicBarrier barrera = new CyclicBarrier(1);
-        Nucleo n1 = new Nucleo(1, 20, busInstrucciones, barrera);
-        Nucleo n2 = new Nucleo(2, 20, busInstrucciones, barrera);
-        
-        //Empezamos a simular!
-        int reloj=0;
-        //Booleanos que determinan si N1 y N2 están ejecutando algo
-        boolean finN1=false;
-        boolean finN2=false;
-        //Cargamos los hilos iniciales en N1 y N2
-        if(!colaEjecucion.isEmpty()) {
-            EstructuraHilo t1 = colaEjecucion.poll();
-            n1.setEstHilo(t1);
-            n1.setTerminado(false);
-        } else {
-            n1.setTerminado(true);
-        }
-        if(!colaEjecucion.isEmpty()) {
-            EstructuraHilo t2 = colaEjecucion.poll();
-            n2.setEstHilo(t2);
-            n2.setTerminado(false);
-        } else {
-            n2.setTerminado(true);
-        }
-        System.out.println("HID-N1: "+n1.getEstHilo().getHid()+", HPC-N1: "+n1.getEstHilo().getHpc());
-        System.out.println("HID-N2: "+n2.getEstHilo().getHid()+", HPC-N2: "+n2.getEstHilo().getHpc());
-        //Tiempo de cada CPU inicia en 0
-        int tiempo1=0;
-        int tiempo2=0;
-        //Si la cola no está vacía o aún se está ejecutando algún hilo, continúe ejecutando
-        while(!colaEjecucion.isEmpty()||!(finN1&&finN2)) {
-            System.out.println("Reloj: "+reloj);
-            
-            if(finN1&&finN2) {//Si ambos núcleos terminaron, carguemos hilos en cada uno (de ser posible)
-                if(!colaEjecucion.isEmpty()) {
-                    EstructuraHilo t1 = colaEjecucion.poll();
-                    n1.setEstHilo(t1);
-                    n1.setTerminado(false);
-                } else {
-                    n1.setTerminado(true);
-                    System.out.println("cola empty, n1");
-                }
-                if(!colaEjecucion.isEmpty()) {
-                    EstructuraHilo t2 = colaEjecucion.poll();
-                    n2.setEstHilo(t2);
-                    n2.setTerminado(false);
-                } else {
-                    n2.setTerminado(true);
-                    System.out.println("cola empty, n2");
-                }
-                System.out.println("HID-N1: "+n1.getEstHilo().getHid()+", HPC-N1: "+n1.getEstHilo().getHpc());
-                System.out.println("HID-N2: "+n2.getEstHilo().getHid()+", HPC-N2: "+n2.getEstHilo().getHpc());
-                tiempo1=0;
-                tiempo2=0;
-            } else if(finN1) {//Si solo N1 terminó, cargue solo N1
-                if(!colaEjecucion.isEmpty()) {
-                    EstructuraHilo t1 = colaEjecucion.poll();
-                    n1.setEstHilo(t1);
-                    n1.setTerminado(false);
-                } else {
-                    n1.setTerminado(true);
-                }
-                System.out.println("HID-N1: "+n1.getEstHilo().getHid()+", HPC-N1: "+n1.getEstHilo().getHpc());
-                System.out.println("HID-N2: "+n2.getEstHilo().getHid()+", HPC-N2: "+n2.getEstHilo().getHpc());
-                tiempo1=0;
-            } else if(finN2) {//Si solo N2 terminó, cargue solo N2
-                if(!colaEjecucion.isEmpty()) {
-                    EstructuraHilo t2 = colaEjecucion.poll();
-                    n2.setEstHilo(t2);
-                    n2.setTerminado(false);
-                } else {
-                    n2.setTerminado(true);
-                }
-                System.out.println("HID-N1: "+n1.getEstHilo().getHid()+", HPC-N1: "+n1.getEstHilo().getHpc());
-                System.out.println("HID-N2: "+n2.getEstHilo().getHid()+", HPC-N2: "+n2.getEstHilo().getHpc());
-                tiempo2=0;
-            } 
-            if(n1.isTerminado()) {//Si el núcleo 1 no tiene nada que cargar, reinicie su tiempo (->no se salga en nop)
-                tiempo1=0;
-            }
-            if(n2.isTerminado()) {//Si el núcleo 2 no tiene nada que cargar, reinicie su tiempo (->no se salga en nop)
-                tiempo2=0;
-            }
-        
-            finN1=false;
-            finN2=false;
-            System.out.println("Nucleo 1: "+n1+"\n"+"Nucleo 2: "+n2);
-            while(n1.Execute()&&n2.Execute()) {//Ejecuto instrucción por instrucción
-                reloj++;
-                tiempo1++;
-                tiempo2++;
-                if((tiempo1>=n1.getQuantum())&&!n1.isTerminado()) {//Si se acaba el quantum y no se ha terminado, guarde el hilo
-                    System.out.println("SE ACABÓ EL QUANTUM DE N1");
-                    n1.guardaHilo();
-                    colaEjecucion.add(n1.getEstHilo());
-                    finN1=true;
-                } 
-                if(tiempo2>=n2.getQuantum()&&!n2.isTerminado()) {//Si se acaba el quantum y no se ha terminado, guarde el hilo
-                    System.out.println("SE ACABÓ EL QUANTUM DE N2");
-                    n2.guardaHilo();
-                    colaEjecucion.add(n2.getEstHilo());
-                    finN2=true;
-                } 
-                if(finN1||finN2) {//Si se acabó uno de los dos quantums, salga del ciclo
-                    break;
-                }
-            }
-            //Determino si debo seguir o no el ciclo externo
-            finN1=!n1.isNoFin()||(tiempo1>=n1.getQuantum())||n1.isTerminado();
-            finN2=!n2.isNoFin()||(tiempo2>=n2.getQuantum())||n2.isTerminado();
-            System.out.println("Nucleo 1: "+n1+"\n"+"Nucleo 2: "+n2);
-            System.out.println("________________________________");
-        }
-        
-        
+        return mem;
     }
     
-
-    
+    //Lee cada archivo de hilo para meterlo en la memoria
     public static ArrayList<Integer> leeArchivo(File archivo) {
         BufferedReader br = null;
 
